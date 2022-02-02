beaa71af9ee04373c3af18643e5426728c4772a9
canepro <mogah.vincent@hotmail.com>
2022-02-02 22:29:29 +0100

    changed project image back 1

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     title: 'Survey Form',
     description: "Using HTLM, CSS you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
-      image: '/images/5.png',
+      image: '/images/1.png',
       tags: ['HTML', 'CSS'],
     source: 'https://github.com/Canepro/FCC-Survey-Form',
     visit: 'https://codepen.io/canepro/pen/vYemNmm',
