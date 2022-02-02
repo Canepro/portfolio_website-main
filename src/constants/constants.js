a67025884fa7bedc81ef07313b1b06f65586f986
canepro <mogah.vincent@hotmail.com>
2022-02-02 22:21:01 +0100

    added a project

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,11 +1,11 @@
 export const projects = [
   {
-    title: 'MERN Memories',
-    description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
-      image: '/images/1.png',
-      tags: ['Mongo', 'Express', 'React', 'Node'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    title: 'Survey Form',
+    description: "Using HTLM, CSS you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
+      image: '/images/5.png',
+      tags: ['HTML', 'CSS'],
+    source: 'https://github.com/Canepro/FCC-Survey-Form',
+    visit: 'https://codepen.io/canepro/pen/vYemNmm',
     id: 0,
   },
   {
