30df0f4d7199804a0c8d98a060675b613e9ce397
canepro <mogah.vincent@hotmail.com>
2022-02-09 00:04:51 +0100

    Updated my accomplishments

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -4,10 +4,10 @@ import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalCompon
 import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
 
 const data = [
-  { number: 20, text: 'Open Source Projects'},
-  { number: 1000, text: 'Students', },
-  { number: 1900, text: 'Github Followers', },
-  { number: 5000, text: 'Github Stars', }
+  { number: 6, text: 'Open Source Projects'},
+  { number: 6, text: 'Certifications', },
+  { number: 1, text: 'Github Followers', },
+  { number: 10, text: 'Github Stars', }
 ];
 
 const Acomplishments = () => (
