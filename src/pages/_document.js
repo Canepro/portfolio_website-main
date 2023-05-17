cbe28df43fb06f4d09bbb377fc67472702689b9e
Vincent Mogah <mogah.vincent@hotmail.com>
2023-05-17 17:21:48 +0100

    Added Livechat to document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -36,6 +36,17 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+
+        <!-- Start of Rocket.Chat Livechat Script -->
+          <script type="text/javascript">
+          (function(w, d, s, u) {
+            w.RocketChat = function(c) { w.RocketChat._.push(c) }; w.RocketChat._ = []; w.RocketChat.url = u;
+            var h = d.getElementsByTagName(s)[0], j = d.createElement(s);
+            j.async = true; j.src = 'https://canepros.rocket.chat/livechat/rocketchat-livechat.min.js?_=201903270000';
+            h.parentNode.insertBefore(j, h);
+          })(window, document, 'script', 'https://canepros.rocket.chat/livechat');
+	      </script>
+
         </body>
       </Html>
     );
