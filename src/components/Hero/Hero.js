939c9ed7d27020753e4689516a62bb045b15ec48
Vincent Mogah <mogah.vincent@hotmail.com>
2023-05-17 13:15:49 +0100

    Fixed error in hero section.

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
-import { Section, SectionText, SectionTitle, Button } from '../../styles/GlobalComponents';
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = () => (
-  <Section row nopadding>
-    <LeftSection>
-      <SectionTitle main center>
-        Welcome
-        <br />
-        I am Vincent Mogah
-      </SectionTitle>
-      <SectionText>
+const Hero = (props) => (
+  <>
+    <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+          Welcome<br />
+          I am Vincent Mogah
+        </SectionTitle>
+        <SectionText>
         I am a DevOps Engineer with a strong foundation in cloud administration and web development. I am a quick learner and I am eager to learn new technologies. I am also a team player and I am able to work effectively with others. I am confident that I can contribute to your company's success.
         <br />
         Here are some of my skills and experience:
@@ -25,9 +26,10 @@ const Hero = () => (
         </ul>
         I am a highly motivated and results-oriented individual. I am confident that I can make a significant contribution to your company. I am eager to learn more about your company and the position you are seeking to fill. Thank you for your time and consideration.
       </SectionText>
-      <Button onClick={() => window.location = 'https://www.linkedin.com/in/vincent-mogah/'}>Learn More</Button>
-    </LeftSection>
-  </Section>
+        <Button onClick={() => window.location = 'https://www.linkedin.com/in/vincent-mogah/'}>Learn More</Button>
+      </LeftSection>
+    </Section>
+  </>
 );
 
 export default Hero;
