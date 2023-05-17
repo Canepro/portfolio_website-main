e8132c06aeff6b78ddf09f2aff7ebd37746a9c7d
Vincent Mogah <mogah.vincent@hotmail.com>
2023-05-17 16:05:41 +0100

    More changes to hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,19 +13,11 @@ const Hero = () => {
           I am Vincent Mogah
         </SectionTitle>
         <SectionText>
-          I am a DevOps Engineer with cloud administration and web development skills. I learn new technologies quickly and work well with others. I can help your company succeed by:
-          <ul>
-            <li>Building and maintaining cloud infrastructure with Terraform and Ansible</li>
-            <li>Automating infrastructure and application deployments with Jenkins and GitLab CI/CD</li>
-            <li>Monitoring and troubleshooting infrastructure and applications with Prometheus and Grafana</li>
-            <li>Deploying and managing applications in Microsoft Azure and AWS</li>
-            <li>Developing user interfaces and web applications with HTML, CSS, and React.js</li>
-            <li>Communicating effectively with clients and explaining technical concepts clearly</li>
-            <li>Identifying and solving problems creatively and critically</li>
-            <li>Collecting and analyzing data to identify trends and make recommendations</li>
-            <li>Contributing to the development and improvement of best practices, procedures, and policies</li>
-          </ul>
-          I am motivated and results-oriented. I am interested in learning more about your company and the position. Thank you for your time and consideration.
+        I am a DevOps Engineer with 5+ years of experience in cloud administration and web development. I am a quick learner and I am eager to learn new technologies. I am also a team player and I am able to work effectively with others. I am confident that I can contribute to your company's success. <br />
+
+        I have experience with Microsoft Azure and AWS, and I am able to deploy and manage applications in the cloud. I am also able to configure and manage cloud resources. I have experience with HTML, CSS, and React.js, and I am able to develop user interfaces and web applications. I am also able to integrate web applications with cloud services. <br />
+
+        I am a highly motivated and results-oriented individual. I am confident that I can make a significant contribution to your company. I am eager to learn more about your company and the position you are seeking to fill. Thank you for your time and consideration. <br />
         </SectionText>
         <Button onClick={() => window.location = 'https://www.linkedin.com/in/vincent-mogah/'}>Learn More</Button>
       </LeftSection>
