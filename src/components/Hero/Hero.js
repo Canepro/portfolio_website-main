f7c61db0d64dd7159bd0c5842e1894003a67e030
Vincent Mogah <mogah.vincent@hotmail.com>
2023-05-17 16:13:11 +0100

    Added paragraphs to hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,12 +13,13 @@ const Hero = () => {
           I am Vincent Mogah
         </SectionTitle>
         <SectionText>
-        I am a DevOps Engineer with 5+ years of experience in cloud administration and web development. I am a quick learner and I am eager to learn new technologies. I am also a team player and I am able to work effectively with others. I am confident that I can contribute to your company's success. <br />
+          <p>I am a DevOps Engineer with 5+ years of experience in cloud administration and web development. I am a quick learner and I am eager to learn new technologies. I am also a team player and I am able to work effectively with others. I am confident that I can contribute to your company's success.</p>
 
-        I have experience with Microsoft Azure and AWS, and I am able to deploy and manage applications in the cloud. I am also able to configure and manage cloud resources. I have experience with HTML, CSS, and React.js, and I am able to develop user interfaces and web applications. I am also able to integrate web applications with cloud services. <br />
+          <p>I have experience with Microsoft Azure and AWS, and I am able to deploy and manage applications in the cloud. I am also able to configure and manage cloud resources. I have experience with HTML, CSS, and React.js, and I am able to develop user interfaces and web applications. I am also able to integrate web applications with cloud services.</p>
 
-        I am a highly motivated and results-oriented individual. I am confident that I can make a significant contribution to your company. I am eager to learn more about your company and the position you are seeking to fill. Thank you for your time and consideration. <br />
+          <p>I am a highly motivated and results-oriented individual. I am confident that I can make a significant contribution to your company. I am eager to learn more about your company and the position you are seeking to fill. Thank you for your time and consideration.</p>
         </SectionText>
+
         <Button onClick={() => window.location = 'https://www.linkedin.com/in/vincent-mogah/'}>Learn More</Button>
       </LeftSection>
     </Section>
