3b48a85bccb5473b6cb41e04e42433dc0f000286
Vincent Mogah <mogah.vincent@hotmail.com>
2023-05-20 02:42:39 +0100

    Made some changes to the index.js to resolve the livechat issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,19 +7,47 @@ import Timeline from '../components/TimeLine/TimeLine';
 import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
 
-const Home = () => {
-  return (
-    <Layout>
-      <Section grid>
-        <Hero />
-        <BgAnimation />
-      </Section>
-      <Projects />
-      <Technologies />
-      <Timeline />
-      <Acomplishments />
-    </Layout>
-  );
-};
+import React, { useEffect } from 'react';
 
+const Home = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://canepros.rocket.chat/livechat/rocketchat-livechat.min.js?_=201903270000';
+    script.async = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+  <Layout>
+    <Section grid>
+      <Hero />
+      <BgAnimation />
+    </Section>
+    <Projects />
+    <Technologies />
+    <Timeline />
+    <Acomplishments />
+    <script
+      type="text/javascript"
+      dangerouslySetInnerHTML={{
+        __html: `
+          (function(w, d, s, u) {
+            w.RocketChat = function(c) { w.RocketChat._.push(c) };
+            w.RocketChat._ = [];
+            w.RocketChat.url = u;
+            var h = d.getElementsByTagName(s)[0],
+              j = d.createElement(s);
+            j.async = true;
+            j.src = 'https://canepros.rocket.chat/livechat/rocketchat-livechat.min.js?_=201903270000';
+            h.parentNode.insertBefore(j, h);
+          })(window, document, 'script', 'https://canepros.rocket.chat/livechat');
+        `,
+      }}
+    />
+  </Layout>
+);
+};
 export default Home;
