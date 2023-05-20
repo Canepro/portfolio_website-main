fffc1d8d18030af204f8bb1f440e0ce7821643bb
Vincent Mogah <mogah.vincent@hotmail.com>
2023-05-20 04:22:04 +0100

    more changes to index.js and document.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -50,23 +50,7 @@ const Home = () => {
       <Technologies />
       <Timeline />
       <Acomplishments />
-      <script
-        type="text/javascript"
-        dangerouslySetInnerHTML={{
-          __html: `
-          (function(w, d, s, u) {
-          w.RocketChat = function(c) { w.RocketChat._.push(c) };
-          w.RocketChat._ = [];
-          w.RocketChat.url = u;
-          var h = d.getElementsByTagName(s)[0],
-          j = d.createElement(s);
-          j.async = true;
-          j.src = 'https://canepros.rocket.chat/livechat/rocketchat-livechat.min.js?_=201903270000';
-          h.parentNode.insertBefore(j, h);
-          })(window, document, 'script', 'https://canepros.rocket.chat/livechat');
-          `,
-        }}
-      />
+
     </Layout>
   );
 };
