3ce8e795eed4928934bc9c8c66beb4616185bdcc
Vincent Mogah <mogah.vincent@hotmail.com>
2023-05-20 03:13:55 +0100

    Change the document.js file back

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,63 +1,62 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
-export default function MyDocument(props) {
-  return (
-    <Html lang='en-GB'>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-        <Script
-          src="https://canepros.rocket.chat/livechat/rocketchat-livechat.min.js?_=201903270000"
-          strategy="lazyOnload"
-        >
-          {`
-            (function(w, d, s, u) {
-              w.RocketChat = function(c) { w.RocketChat._.push(c) };
-              w.RocketChat._ = [];
-              w.RocketChat.url = u;
-              var h = d.getElementsByTagName(s)[0],
-              j = d.createElement(s);
-              j.async = true;
-              j.src = 'https://canepros.rocket.chat/livechat/rocketchat-livechat.min.js?_=201903270000';
-              h.parentNode.insertBefore(j, h);
-            })(window, document, 'script', 'https://canepros.rocket.chat/livechat');
-          `}
-        </Script>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-}
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
 
-MyDocument.getInitialProps = async (ctx) => {
-  const sheet = new ServerStyleSheet()
-  const originalRenderPage = ctx.renderPage
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
 
-  try {
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: (App) => (props) =>
-          sheet.collectStyles(<App {...props} />),
-      })
-
-    const initialProps = await Document.getInitialProps(ctx)
-    return {
-      ...initialProps,
-      styles: (
-        <>
-          {initialProps.styles}
-          {sheet.getStyleElement()}
-        </>
-      ),
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
     }
-  } finally {
-    sheet.seal()
+  }
+
+  render() {
+    return (
+      <Html lang='en-GB'>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+          <script type="text/javascript">
+            {`
+              (function(w, d, s, u) {
+                w.RocketChat = function(c) { w.RocketChat._.push(c) };
+                w.RocketChat._ = [];
+                w.RocketChat.url = u;
+                var h = d.getElementsByTagName(s)[0],
+                  j = d.createElement(s);
+                j.async = true;
+                j.src = 'https://canepros.rocket.chat/livechat/rocketchat-livechat.min.js?_=201903270000';
+                h.parentNode.insertBefore(j, h);
+              })(window, document, 'script', 'https://canepros.rocket.chat/livechat');
+            `}
+          </script>
+          
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
-
