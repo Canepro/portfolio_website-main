3e6f6721080dc96e07d4c2456aa9382b9ce88369
Vincent Mogah <mogah.vincent@hotmail.com>
2023-05-20 03:07:51 +0100

    Made extra change to index.js and document.js for the livechat issue

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,62 +1,63 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
-export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+export default function MyDocument(props) {
+  return (
+    <Html lang='en-GB'>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+        <Script
+          src="https://canepros.rocket.chat/livechat/rocketchat-livechat.min.js?_=201903270000"
+          strategy="lazyOnload"
+        >
+          {`
+            (function(w, d, s, u) {
+              w.RocketChat = function(c) { w.RocketChat._.push(c) };
+              w.RocketChat._ = [];
+              w.RocketChat.url = u;
+              var h = d.getElementsByTagName(s)[0],
+              j = d.createElement(s);
+              j.async = true;
+              j.src = 'https://canepros.rocket.chat/livechat/rocketchat-livechat.min.js?_=201903270000';
+              h.parentNode.insertBefore(j, h);
+            })(window, document, 'script', 'https://canepros.rocket.chat/livechat');
+          `}
+        </Script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        })
+MyDocument.getInitialProps = async (ctx) => {
+  const sheet = new ServerStyleSheet()
+  const originalRenderPage = ctx.renderPage
 
-      const initialProps = await Document.getInitialProps(ctx)
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      }
-    } finally {
-      sheet.seal()
+  try {
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) =>
+          sheet.collectStyles(<App {...props} />),
+      })
+
+    const initialProps = await Document.getInitialProps(ctx)
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          {sheet.getStyleElement()}
+        </>
+      ),
     }
-  }
-
-  render() {
-    return (
-      <Html lang='en-GB'>
-        <Head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
-            rel="stylesheet"
-          />
-          <script type="text/javascript">
-            {`
-              (function(w, d, s, u) {
-                w.RocketChat = function(c) { w.RocketChat._.push(c) };
-                w.RocketChat._ = [];
-                w.RocketChat.url = u;
-                var h = d.getElementsByTagName(s)[0],
-                  j = d.createElement(s);
-                j.async = true;
-                j.src = 'https://canepros.rocket.chat/livechat/rocketchat-livechat.min.js?_=201903270000';
-                h.parentNode.insertBefore(j, h);
-              })(window, document, 'script', 'https://canepros.rocket.chat/livechat');
-            `}
-          </script>
-          
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
+  } finally {
+    sheet.seal()
   }
 }
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,44 +10,75 @@ import { Section } from '../styles/GlobalComponents';
 import React, { useEffect } from 'react';
 
 const Home = () => {
-  useEffect(() => {
+  // Define a state variable to store the livechat script
+  const [livechatScript, setLivechatScript] = React.useState(null);
+
+  // Define a function to load the livechat script
+  const loadLivechatScript = () => {
+    // Create a script element
     const script = document.createElement('script');
+
+    // Set the source and attributes of the script
     script.src = 'https://canepros.rocket.chat/livechat/rocketchat-livechat.min.js?_=201903270000';
     script.async = true;
+    script.id = 'livechat-script';
+
+    // Append the script to the document body
     document.body.appendChild(script);
+
+    // Set the livechat script state variable to the script element
+    setLivechatScript(script);
+  };
+
+  // Define a function to unload the livechat script
+  const unloadLivechatScript = () => {
+    // Remove the script element from the document body
+    document.body.removeChild(livechatScript);
+
+    // Set the livechat script state variable to null
+    setLivechatScript(null);
+  };
+
+  // Use an effect hook to load and unload the livechat script on mount and unmount
+  useEffect(() => {
+    // Load the livechat script on mount
+    loadLivechatScript();
+
+    // Return a cleanup function to unload the livechat script on unmount
     return () => {
-      document.body.removeChild(script);
+      unloadLivechatScript();
     };
   }, []);
 
   return (
-  <Layout>
-    <Section grid>
-      <Hero />
-      <BgAnimation />
-    </Section>
-    <Projects />
-    <Technologies />
-    <Timeline />
-    <Acomplishments />
-    <script
-      type="text/javascript"
-      dangerouslySetInnerHTML={{
-        __html: `
+    <Layout>
+      <Section grid>
+        <Hero />
+        <BgAnimation />
+      </Section>
+      <Projects />
+      <Technologies />
+      <Timeline />
+      <Acomplishments />
+      <script
+        type="text/javascript"
+        dangerouslySetInnerHTML={{
+          __html: `
           (function(w, d, s, u) {
-            w.RocketChat = function(c) { w.RocketChat._.push(c) };
-            w.RocketChat._ = [];
-            w.RocketChat.url = u;
-            var h = d.getElementsByTagName(s)[0],
-              j = d.createElement(s);
-            j.async = true;
-            j.src = 'https://canepros.rocket.chat/livechat/rocketchat-livechat.min.js?_=201903270000';
-            h.parentNode.insertBefore(j, h);
+          w.RocketChat = function(c) { w.RocketChat._.push(c) };
+          w.RocketChat._ = [];
+          w.RocketChat.url = u;
+          var h = d.getElementsByTagName(s)[0],
+          j = d.createElement(s);
+          j.async = true;
+          j.src = 'https://canepros.rocket.chat/livechat/rocketchat-livechat.min.js?_=201903270000';
+          h.parentNode.insertBefore(j, h);
           })(window, document, 'script', 'https://canepros.rocket.chat/livechat');
-        `,
-      }}
-    />
-  </Layout>
-);
+          `,
+        }}
+      />
+    </Layout>
+  );
 };
+
 export default Home;
