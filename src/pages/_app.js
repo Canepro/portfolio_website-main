ed74f21f21fa3a592a8548989d63ae65b534ef01
Vincent Mogah <mogah.vincent@hotmail.com>
2025-05-24 03:49:14 +0100

    feat: simplify MyApp component by removing unnecessary Theme wrapper

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { ThemeProvider } from '../contexts/ThemeContext';
-import Theme from '../styles/theme';
 import '../styles/globals.css'; // if you have global CSS
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider>
-      <Theme>
-        <Component {...pageProps} />
-      </Theme>
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 }
